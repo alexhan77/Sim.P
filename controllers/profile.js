46841864b69d1afca9a6b2a6432f4fc30d5ae02f
Jummay77 <hansangho@yahoo.com>
2020-04-30 16:16:55 -0700

    All funtionality works

--- controllers/profile.js
+++ controllers/profile.js
@@ -17,41 +17,58 @@ router.get('/user', (req, res) => {
 // GET /profile/guest/userID - viewing a user's profile as a guest
 router.get('/guest/:id', (req, res) => {
     db.user.findByPk(req.params.id)
-    .then(userProfile => {
-        res.render('profile/guest', { moment, userProfile })
-    })
-    .catch(err => {
-        res.render('error', err)
-    })
+        .then(userProfile => {
+            res.render('profile/guest', { moment, userProfile })
+        })
+        .catch(err => {
+            res.render('error', err)
+        })
 })
 
 // GET /profile/admin - a special profile admins
 // NOTE: Protect this route from users who are not logged in AND users who are NOT admins
 router.get('/admin', adminLogin, (req, res) => {
     db.user.findAll()
-    .then(user => {
-        res.render('profile/admin', { moment, users })
+        .then(user => {
+            res.render('profile/admin', { moment, users })
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error')
+        })
+})
+
+router.get('/edit/:id', (req, res) => {
+    db.user.findOne({
+        where: { id: req.params.id }
     })
-    .catch(err => {
-        console.log(err)
-        res.render('error')
-    })
+        .then(user => {
+            res.render('profile/edit', { user })
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error boi')
+        })
 })
 
 // PUT ROUTES -editting the profile page
-router.put('/:id', (req, res) => {
+router.put('/edit/:id', (req, res) => {
     console.log('REQUEST BODY', req.body)
     db.user.update(
-        req.body,
-        { where: { id: req.params.id } }
-    )
-    .then(() => {
-        res.redirect('/user/' + req.params.id)
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('error')
-    })
+        {
+            firstname: req.body.firstname, lastname: req.body.lastname, username: req.body.username,
+            pic: req.body.pic, bio: req.body.bio
+        },
+        {
+            where: { id: req.params.id }
+        })
+        .then(() => {
+            res.redirect('/profile/user')
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error')
+        })
 })
 
-module.exports  = router
+module.exports = router
